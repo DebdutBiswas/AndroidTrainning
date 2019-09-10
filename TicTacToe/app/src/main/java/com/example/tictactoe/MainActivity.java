f6f4b968af4ef8669863ec50e555795e8b16a71d
Debdut <rupam.biswas@outlook.com>
2019-09-10 20:09:07 +0530

    Tic Tac Toe User Game Logic

--- TicTacToe/app/src/main/java/com/example/tictactoe/MainActivity.java
+++ TicTacToe/app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -2,14 +2,15 @@ package com.example.tictactoe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     String activePlayer = "H"; //H: Human, C: Computer
-    String[] gameState = {"E","E","E","E","E","E","E","E","E"}; //E: Empty, R: Red, Y: Yellow
-    int[][] winningState = {{0,1,2}, /* ### */
+    String[] gameStates = {"E","E","E","E","E","E","E","E","E"}; //E: Empty, R: Red, Y: Yellow
+    int[][] winningStates = {{0,1,2}, /* ### */
                             {3,4,5}, /* ### */
                             {6,7,8}, /* ### */
 
@@ -35,21 +36,37 @@ public class MainActivity extends AppCompatActivity {
         ImageView capImageView = (ImageView) capView;
         int currentTappedCap = Integer.parseInt(capImageView.getTag().toString());
 
-        if(gameState[currentTappedCap] == "E") {
+        if(gameStates[currentTappedCap] == "E") {
             capImageView.setAlpha(0f);
             capImageView.setRotation(-3600);
 
             if (activePlayer == "H") {
                 capImageView.setImageResource(R.drawable.yellow);
                 activePlayer = "C";
-                gameState[currentTappedCap] = "Y";
+                gameStates[currentTappedCap] = "Y";
             } else {
                 capImageView.setImageResource(R.drawable.red);
                 activePlayer = "H";
-                gameState[currentTappedCap] = "R";
+                gameStates[currentTappedCap] = "R";
             }
 
             capImageView.animate().alpha(1f).setDuration(300).rotation(3600).setDuration(600);
+
+            for(int[] winningState : winningStates){
+                if(gameStates[winningState[0]] == gameStates[winningState[1]] &&
+                        gameStates[winningState[1]] == gameStates[winningState[2]] &&
+                        gameStates[winningState[0]] != "E"){
+
+                    Log.i("Info: ",gameStates[winningState[0]]);
+                    for(int winningStateChunk : winningState) {
+                        //animateCapView(findViewById(getResources().getIdentifier("capImageView" + winningStateChunk,"id",getPackageName())));
+                        findViewById(getResources().getIdentifier("capImageView" + winningStateChunk,"id",getPackageName())).setAlpha(0f);
+                        findViewById(getResources().getIdentifier("capImageView" + winningStateChunk,"id",getPackageName())).setScaleX(0.5f);
+                        findViewById(getResources().getIdentifier("capImageView" + winningStateChunk,"id",getPackageName())).setScaleY(0.5f);
+                        findViewById(getResources().getIdentifier("capImageView" + winningStateChunk,"id",getPackageName())).animate().alpha(1f).setDuration(300).scaleX(1f).scaleY(1f).setDuration(600);
+                    }
+                }
+            }
         }
     }
 
