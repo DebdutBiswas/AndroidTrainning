13494e7f86036b0c1ebbc377482624ba6bf51224
Debdut <rupam.biswas@outlook.com>
2019-10-02 23:00:00 +0530

    Audio Demo Mid Commit

--- AudioDemo/app/src/main/java/com/example/audiodemo/MainActivity.java
+++ AudioDemo/app/src/main/java/com/example/audiodemo/MainActivity.java
@@ -4,14 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button playBtn;
+    //Button pauseBtn = findViewById(R.id.pauseBtn);
+    public MediaPlayer demoMediaPlayer;
+
+    public void playAudio(View view) {
+        //Button playBtn = findViewById(R.id.playBtn);
+
+        if(!demoMediaPlayer.isPlaying()) {
+            demoMediaPlayer.start();
+            playBtn.setText("Pause");
+        } else {
+            demoMediaPlayer.pause();
+            playBtn.setText("Play");
+        }
+    }
+
+    public void pauseAudio(View view) {
+        if(demoMediaPlayer.isPlaying()) {
+            demoMediaPlayer.pause();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MediaPlayer demoMediaPlayer = MediaPlayer.create(this, R.raw.bell);
+        playBtn = findViewById(R.id.playBtn);
+        demoMediaPlayer = MediaPlayer.create(this, R.raw.bell);
     }
 }
